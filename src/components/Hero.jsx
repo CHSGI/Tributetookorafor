847c0242c40d4b00bd10491f3f3e21678bb478f3
ksohlo <alabssouls@gmail.com>
2024-08-02 16:51:13 +0100

    final touch

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,27 @@
 import bgImage from "../assets/img/bg.png";
 import baba from "../assets/img/baba.png"
 import flower from "../assets/img/flower.png"
+import { useState } from "react";
 
 const Hero = () => {
+  
+  const [modalOpen, setModalOpen] = useState(false);
   return (
+  <>
+    {modalOpen && 
+            <div className=' fixed top-0 w-full h-screen backdrop-blur-lg flex justify-center items-center z-50'>
+            <div className='w-4/5 lg:w-1/2 h-auto lg:h-3/4 rounded-2xl bg-white p-7'>
+              <p className='cursor-pointer font-semibold text-customBrown text-2xl text-right' onClick={()=>{setModalOpen(false)}}>&times;</p>
+              <p className='font-serif text-2xl m-3 text-customBrown'>Leave your Tribute</p>
+              <form action="">
+                <input type="text" id='name' name='name' placeholder='FULL NAME' className=' w-full mt-5 mx-auto block outline-none border-customGold border-2 rounded-xl p-2 text-sm' />
+                <textarea name="tribute" rows="5" cols={"50"}  id="Tribute" placeholder='ADD YOUR TRIBUTE TEXT HERE' className='mt-5 mx-auto w-full block outline-none p-2 border-customGold border-2 rounded-xl text-sm'></textarea>
+                <button className=' p-3 bg-customGold rounded-full text-white text-xs mt-4 font-medium'>
+                  PUBLISH TRIBUTE</button>
+              </form>
+            </div> 
+          </div>
+    }
     <div
       className="relative h-screen"
       style={{
@@ -22,9 +40,12 @@ const Hero = () => {
           <h1 className="text-3xl font-serif font-extralight tracking-tight text-customBrown sm:text-6xl">
             EMMANUEL OKORAFOR
           </h1>
+          <p className="text-xs text-customBrown font-serif">
+            16 JUNE 1957 - 15 JUNE 2024</p>
           <div className="flex items-center justify-center mt-10 gap-x-6">
             <a
               href="#"
+              onClick={()=>{setModalOpen(true)}}
               className="px-4 py-3 text-sm font-light text-white bg-customGold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
               style={{ borderRadius: "20px" }}
             >
@@ -38,6 +59,7 @@ const Hero = () => {
         </div>
       </div>
     </div>
+  </>
   );
 };
 
--- src/components/Tribute.jsx
+++ src/components/Tribute.jsx
@@ -3,25 +3,30 @@ import BabaGrey from "../assets/img/babaGrey.png"
 
 
 export default function Tribute() {
-    const [modalOpen, setModalOpen] = useState(false)
+    const [modalOpen, setModalOpen] = useState(false);
     const[tribute, setTribute] = useState([
         {
+            key:1,
             name: "Chinwe Nwoke",
             description: "Dearest Emmanuel \n So sad you're no more, but God knows best, as He gives and takes. Will be there for Amanda & Osi. May God preserve you in His eternal life till we meet again, a precious friend and sister. Love you loads.God's comfort of love to your lovely family and friends.... read more",
         },
         {
+            key: 2,
             name: "Athan Nwadike",
             description: "Dear Emmanuel, \n Words fail me. Rest in God till resurrection day and may God comfort your family."
         },
         {
+            key:3,
             name: "Josephine Yul",
             description: "One more angel in heaven…."
         },
         {
+            key:4,
             name: "Alps Yemi",
             description: "Emmanuel is truly gone?This reality is so hard to take in. We prayed so hard for her recovery, God knows we prayed. But God has chosen the very best for Himself. Ose was indeed the best in every imaginable way. Just like Augusta said, she was always peaceful right from childhood. As a young child, wherever you asked her to stay is exactly where you would find her, as long as she had her thumb in her mouth.Ose was gentle, kind, generous, thoughtful,... read more"
         },
         {
+            key:5,
             name: "Deborah O.",
             description: "Dear Ose,\n I never met you but I prayed so much for your cure. I thank God for all the help you received during your illness and the fact that you received the last sacraments. May God continue to bless and strengthen your loved ones. You fought the good fight!"
         },

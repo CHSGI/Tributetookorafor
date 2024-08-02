a23638e541108b2ee96afbb52e870b58ae43f959
great <greatoladipupo818@gmail.com>
2024-08-02 11:27:59 +0100

    Header desktop view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,42 @@
-import bgImage from '../assets/img/bg.png';
+import bgImage from "../assets/img/bg.png";
+import baba from "../assets/img/baba.png"
 
 const Hero = () => {
   return (
-    <div className="relative h-screen" style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'top center' }}>
-      {/* Overlay */}
+    <div
+      className="relative h-screen"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "top center",
+      }}
+    >
 
-      <div className="relative flex items-center justify-center h-full px-6 isolate lg:px-8">
-        <div className="max-w-2xl text-center">
+      <div className="relative flex items-center sm:items-end h-full px-6 isolate lg:px-8">
+        <div className="max-w-2xl text-center md:mb-24">
           <h1 className="text-4xl font-bold tracking-tight text-customBrown sm:text-6xl">
-            Tribute to 
+            Tribute to
           </h1>
-          <p className='text-customBrown leading-8 text-lg'>PHARM. DEACON</p>
+          <p className="text-customBrown leading-8 text-lg">PHARM. DEACON</p>
           <h1 className="text-4xl tracking-tight text-customBrown sm:text-6xl">
-          EMMANUEL OKORAFOR
+            EMMANUEL OKORAFOR
           </h1>
           <div className="flex items-center justify-center mt-10 gap-x-6">
             <a
               href="#"
-              className="px-4 py-3 text-sm font-semibold text-white bg-customGold shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-              style={{ borderRadius: '5px' }}
+              className="px-4 py-3 text-sm font-semibold text-white bg-customGold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+              style={{ borderRadius: "20px" }}
             >
               SEND YOUR TRIBUTE
             </a>
           </div>
         </div>
+        <div className="max-w-lg">
+          <img src={baba} alt="Background" className="object-cover w-full h-full " />
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
